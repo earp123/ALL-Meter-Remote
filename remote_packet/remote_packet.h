e5fa4de80a79c2040d5e7dc37cc29217f40b8c00
earp123 <33456310+earp123@users.noreply.github.com>
2024-06-21 15:53:51 -0500

    added remote_packet structs, naming

---
+++ remote_packet/remote_packet.h
@@ -0,0 +1,31 @@
+struct remote_packet {
+  uint16_t lux = 45;
+  uint8_t satsInView = 23;
+  float horizAcc = 0.02;
+  
+  //use 8 decminals
+  double latit  = 45.12345678;
+  double longit = 23.87654321;
+  
+  //from Rx RTC
+  int hour = 12;
+  int minute = 34;
+  int month = 12;
+  int day = 29;
+  int year = 2024;
+  
+  int rxBatt = 9;
+  
+  bool read_done = true;
+    
+};
+enum rx_command {
+	LUX_READ, READ_DONE, PWR_OFF, VEML_PARAM
+};
+
+struct rx_packet {
+	
+	rx_command cmd = READ_DONE;
+	
+	uint8_t data[32] = {0};
+};
